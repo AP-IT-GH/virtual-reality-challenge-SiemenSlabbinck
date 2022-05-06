2f44214ed1dfcbb454ac70cd83064fe46cc03c19
SiemenSlabbinck <siemen.slabbinck@student.ap.be>
2022-05-06 15:39:57 +0200

    Update extras

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public gameObject objToRotate;
+    public gameObject rotation;
+
+    public void Rotate(){
+        
+    }
+}
